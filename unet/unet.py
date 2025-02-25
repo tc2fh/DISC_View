82d248487bc26e06dfa40a7a5ebd6910b6c71bf5
Hilde Younce <hilde.younce@gmail.com>
2025-02-25 17:27:02 -0500

    Edited convolutional block

--- unet/unet.py
+++ unet/unet.py
@@ -9,18 +9,20 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class UNet(nn.Module):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class ConvBlockDown(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        # add batch normalization?
+        self.block_down = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1), # bias true or false?
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2)
+        )
+    
+    def forward(self, x):
+        return self.block_down(x)
 
-    def contracting(self):
-        pass
-
-    def connecting(self):
-        pass
-
-    def expansive(self):
-        pass
-
-    def forward(self):
-        pass
+class Contracting(nn.Module):
+    pass 
