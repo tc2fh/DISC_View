20936515bd8b703d02f14b464bdde86edf43ac12
Dan Luettgen <mrclutchen@gmail.com>
2025-04-01 15:58:56 -0400

    remove the bounding box logic

--- unet/GlaucomaDataset.py
+++ unet/GlaucomaDataset.py
@@ -4,32 +4,24 @@ import torch
 import torchvision.transforms as transforms
 from PIL import Image
 from torch.utils.data import Dataset
-from sklearn.model_selection import train_test_split
-import pandas
+
 
 class GlaucomaDataset(Dataset):
-    def __init__(self, images_path, masks_path, img_filenames, mask_filenames, bbox_df=pd.DataFrame()):
+    def __init__(self, images_path, masks_path, img_filenames, mask_filenames):
         self.images_path = images_path
         self.masks_path = masks_path
         self.img_filenames = img_filenames
         self.mask_filenames = mask_filenames
-        self.images = [] 
+        self.images = []
         self.masks = []
-        self.bboxes = {}
-
-        # Load bounding box data
-        self.bbox_data = bbox_df
-        
-        # Store bounding boxes in a dictionary for quick lookup
-        for _, row in self.bbox_data.iterrows():
-            self.bboxes[row['image_path']] = [row['x1'], row['y1'], row['x2'], row['y2']]
 
         # Pre-processing images: convert to RGB array, convert to tensor, resize
         for img in self.img_filenames:
             img_name = os.path.join(images_path, img)
             img = np.array(Image.open(img_name).convert('RGB'))
             img = transforms.functional.to_tensor(img)
-            img = transforms.functional.resize(img, size=(256,256), interpolation=Image.BILINEAR)
+            img = transforms.functional.resize(
+                img, size=(256, 256), interpolation=Image.BILINEAR)
             self.images.append(img)
 
         # # Pre-processing masks: convert to array, create binary masks, convert to tensors, resize
@@ -38,20 +30,22 @@ class GlaucomaDataset(Dataset):
             mask = np.array(Image.open(mask_name, mode='r'))
 
             # Create binary masks for optic disc and optic cup classes
-            od = (mask>0).astype(np.float32)
-            oc = (mask>1.).astype(np.float32)
+            od = (mask > 0).astype(np.float32)
+            oc = (mask > 1.).astype(np.float32)
 
             # Convert to tensor and add batch dimension
-            od = torch.from_numpy(od[None,:,:]) # (1, Height, Width)
-            oc = torch.from_numpy(oc[None,:,:])
+            od = torch.from_numpy(od[None, :, :])  # (1, Height, Width)
+            oc = torch.from_numpy(oc[None, :, :])
 
             # Resize using nearest neighbor interpolation
-            od = transforms.functional.resize(od, size=(256,256), interpolation=Image.NEAREST)
-            oc = transforms.functional.resize(oc, size=(256,256), interpolation=Image.NEAREST)
+            od = transforms.functional.resize(
+                od, size=(256, 256), interpolation=Image.NEAREST)
+            oc = transforms.functional.resize(
+                oc, size=(256, 256), interpolation=Image.NEAREST)
             self.masks.append(torch.cat([od, oc], dim=0))
 
     def __len__(self):
         return len(self.img_filenames)
-    
+
     def __getitem__(self, idx):
         return self.images[idx], self.img_filenames[idx], self.masks[idx], self.mask_filenames[idx]
