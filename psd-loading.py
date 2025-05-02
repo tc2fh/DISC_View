7962b3b56d99fb10a83125553df13ebd3becc5d3
djl1626clear <mrclutchen@gmail.com>
2025-05-02 12:52:57 -0400

    updated documentation

--- psd-loading.py
+++ psd-loading.py
@@ -8,11 +8,10 @@ from skimage.morphology import skeletonize
 
 
 def fill_contours(layer, is_disc, subject):
-    # 2) to OpenCV BGR
+    # Convert the layer to RGB
     img = cv2.cvtColor(np.array(layer), cv2.COLOR_RGBA2BGR)
 
-    # 3) isolate the “red dots” (assumes they’re pure red)
-    #    you may need to tweak these thresholds if your dots are anti-aliased!
+    # isolate the red annotation if we're getting the disc mask else blue annotations
     if is_disc:
         lower = np.array([0,   0, 200])   # BGR
         upper = np.array([50,  50, 255])
@@ -25,24 +24,26 @@ def fill_contours(layer, is_disc, subject):
 
         mask = cv2.inRange(img, lower, upper)
 
+    # Set the kernel for interpolation
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
 
     # close = dilate then erode → fills gaps
     closed = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
-    # optionally, thin the result back to a 1-pixel wide skeleton
-    skel = skeletonize(closed//255)       # skeletonize wants a 0/1 image
+    # thin the result back to a 1-pixel wide skeleton
+    skel = skeletonize(closed//255)       # skeletonize wants a 0-1 range image
     skel = (skel.astype(np.uint8) * 255)
     contours, _ = cv2.findContours(
         skel, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # 2) make a blank canvas
+    # make a blank "canvas"
     filled = np.zeros_like(closed)
 
-    # 3) draw each contour filled
+    # draw each contour filled
     cv2.drawContours(filled, contours, contourIdx=-1,
                      color=255, thickness=cv2.FILLED)
 
+    # save the interpolated mask
     file_name = f'{subject}-disc.png' if is_disc else f'{subject}-cup.png'
     cv2.imwrite(file_name, filled)
 
